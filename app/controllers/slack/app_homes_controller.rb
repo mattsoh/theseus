028ba2c721ce81cfdd88bbb9751750d9a54938fe
24c02 <163450896+24c02@users.noreply.github.com>
2025-09-05 13:14:29 -0400

    make truncation configurable

--- app/controllers/slack/app_homes_controller.rb
+++ app/controllers/slack/app_homes_controller.rb
@@ -1,14 +1,15 @@
 module Slack
   class AppHomesController < BaseSlackController
     include Public::MailQuerying
+    MAX_MAIL_COUNT = 90 # give us a little headroom...
 
     def handle
       return publish_home_view :no_user unless user_signed_in?
 
       check_the_mail current_public_user.email
 
-      if @mail.count > 95
-        @mail = @mail.first(95)
+      if @mail.count > MAX_MAIL_COUNT
+        @mail = @mail.first(MAX_MAIL_COUNT)
         @truncated = true
       end
 
