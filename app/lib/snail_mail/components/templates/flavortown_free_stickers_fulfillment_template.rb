a906278307e2caa8c063eb6f54ba0282df8a1ed6
24c02 <163450896+24c02@users.noreply.github.com>
2026-01-13 18:06:54 -0500

    what if it was smaller

--- app/lib/snail_mail/components/templates/flavortown_free_stickers_fulfillment_template.rb
+++ app/lib/snail_mail/components/templates/flavortown_free_stickers_fulfillment_template.rb
@@ -42,7 +42,7 @@ module SnailMail
 
           # Render postal elements
           render_imb(240, 24, 183)
-          render_qr_code(5, 115, 60)
+          render_qr_code(5, 115, 50)
           render_letter_id(10, 65, 10, rotate: 90)
           render_postage
         end
